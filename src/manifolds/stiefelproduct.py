d61050a8feb0913a3b3c134c896dfde5af99c958
s-a-barnett <sambarnett95@gmail.com>
2020-03-26 08:59:07 -0400

    stiefelproduct manifold written

---
+++ src/manifolds/stiefelproduct.py
@@ -0,0 +1,51 @@
+from .manifold import Manifold
+import numpy as np
+from numpy.linalg import svd, qr
+
+# helper function to compute symmetric part of matrix X
+def sym(X):
+    return (X + X.T) / 2
+
+class StiefelProduct(Manifold):
+
+    def __init__(self, m, n, r):
+        self.m = m
+        self.n = n
+        self.r = r
+
+    def _reprToPoint(self, xx):
+        return xx[0] @ xx[1].T
+
+    def _inner(self, xx, yy):
+        return np.sum(xx[0]*yy[0]) + np.sum(xx[1]*yy[1])
+
+    def _norm(self, xx):
+        return np.sqrt(self._inner(xx, xx))
+
+    def _retract(self, xx, uu):
+        _, s, vh = svd(xx[0] + uu[0])
+        s_inv = np.diag(1 / s)
+        r_0 = (xx[0] + uu[0]) @ (vh.T @ s_inv @ vh)
+        r_1 = xx[1] + uu[1]
+        return r_0, r_1
+
+    def _project(self, xx, zz):
+        p_0 = zz[0] - (xx[0] @ sym(xx[0].T @ zz[0]))
+        return p_0, zz[1]
+
+    def _riemannianGradient(self, euclideanGradient):
+        return lambda xx: self._project(xx, euclideanGradient(xx))
+
+    def _riemannianHessian(self, euclidenGradient, euclideanHessian):
+
+        def rH(xx, vv):
+            r_0 = self._project(xx, euclideanHessian(xx, vv))[0] - (vv[0] @ sym(xx[0].T @ euclidenGradient(xx)[0]))
+            r_1 = euclideanHessian(xx, vv)[1]
+            return r_0, r_1
+
+        return rH
+
+    def _randomPoint(self):
+        a = np.random.randn(self.m, self.r)
+        q, _ = qr(a)
+        return q, np.random.randn(self.n, self.r)
---
+++ tests/test_stiefelproduct.py
@@ -0,0 +1,46 @@
+import pytest
+import numpy as np
+import sys
+sys.path.append('../')
+
+from src.manifolds.stiefelproduct import StiefelProduct
+
+@pytest.fixture
+def stiefelproduct():
+    m = 10; n = 10; r = 10;
+    return StiefelProduct(m, n, r)
+
+class TestStiefelProduct:
+
+    def test_reprToPoint(self, stiefelproduct):
+        m = stiefelproduct.m; n = stiefelproduct.n; r = stiefelproduct.r;
+
+        L = np.ones((m, r)); R = np.zeros((n, r))
+
+        assert np.allclose(stiefelproduct._reprToPoint((L, R)), np.zeros((m, n)))
+
+    def test_inner(self, stiefelproduct):
+        m = stiefelproduct.m; n = stiefelproduct.n; r = stiefelproduct.r;
+
+        L = np.ones((m, r)); R = np.ones((n, r))
+        xx = (L, R); yy = xx;
+
+        assert stiefelproduct._inner(xx, yy) == r*(m+n)
+
+    def test_retract(self, stiefelproduct):
+        m = stiefelproduct.m
+        xx = (0.25*np.eye(m), np.zeros((m, m)))
+        yy = (np.zeros((m, m)), np.zeros((m, m)))
+        rr = stiefelproduct._retract(xx, yy)
+        assert np.allclose(rr[0], np.eye(m))
+
+    def test_project(self, stiefelproduct):
+        m = stiefelproduct.m
+        Z = np.random.randn(m, m); X = np.random.randn(m, m);
+        rr = stiefelproduct._project((X, X), (Z, Z))
+        r_0 = Z - X @ ((X.T @ Z + Z.T @ X) / 2)
+        assert np.allclose(rr[0], r_0)
+
+    def test_randomPoint(self, stiefelproduct):
+        x, _ = stiefelproduct._randomPoint()
+        assert np.allclose(x.T @ x, np.eye(stiefelproduct.r))
