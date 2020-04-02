fixed hessians

src/costs/regularized_mle.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.linalg import norm
-import scipy.sparse as sparse
+import scipy.sparse.linalg as splinalg
 
 from .cost import Cost
 
@@ -15,7 +15,7 @@ class RegularizedMLE(Cost):
         self.B = B
         self.mu = mu
 
-        self.f = lambda X: 0.5 * (sparse.linalg.norm((B.multiply(X - Y))) ** 2)
+        self.f = lambda X: 0.5 * (splinalg.norm((B.multiply(X - Y))) ** 2)
         self.gradf = lambda X: (B.multiply(X - Y))
 
     def _eval(self, xx):
@@ -32,11 +32,17 @@ class RegularizedMLE(Cost):
 
     def _euclideanHessian(self, xx, hh):
         L, R = xx; U_L, U_R = hh
-        right = (self.B.multiply(L@U_R.T)).T @ L + \
-               (U_R @ ((R.T @ R) - (L.T @ L)))*(self.mu) + \
-               (R @ ((U_R.T@R) + (R.T@U_R)))*(self.mu)
-        left = (self.B.multiply(U_L@R.T)) @ R + \
-               (U_L @ ((L.T @ L) - (R.T @ R)))*(self.mu) + \
-               (L @ ((U_L.T@L) + (L.T@U_L)))*(self.mu)
+
+        left = ((self.B.multiply(U_L@R.T)) @ R) + \
+               ((self.B.multiply(L@U_R.T)) @ R) + \
+               ((self.B.multiply((L@R.T) - self.Y)) @ U_R) + \
+               ((U_L @ ((L.T @ L) - (R.T @ R)))*(self.mu)) + \
+               ((L @ ((U_L.T@L) + (L.T@U_L) - (R.T@U_R) - (U_R.T@R)))*(self.mu))
+
+        right = ((self.B.multiply(L@U_R.T)).T @ L) + \
+                ((self.B.multiply(U_L@R.T)).T @ L) + \
+                ((self.B.multiply((L@R.T) - self.Y)).T @ U_L) + \
+                ((U_R @ ((R.T @ R) - (L.T @ L)))*(self.mu)) + \
+                ((R @ ((U_R.T@R) + (R.T@U_R) - (U_L.T@L) - (L.T@U_L)))*(self.mu))
 
         return left, right